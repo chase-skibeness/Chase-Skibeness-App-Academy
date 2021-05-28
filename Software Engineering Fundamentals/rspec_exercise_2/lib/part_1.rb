57b78232496fafc1d0d1d323f21ea28e2fdf9a74
Chase Skibeness <chaseskibbs@gmail.com>
2021-05-28 14:06:54 -0700

    rspec pt2 work

---
+++ Software Engineering Fundamentals/rspec_exercise_2/lib/part_1.rb
@@ -0,0 +1,33 @@
+def partition(arr, num)
+    rtnArr = [[],[]]
+    arr.each do |ele|
+        if ele < num
+            rtnArr[0] << ele
+        else
+            rtnArr[1] << ele
+        end
+    end
+    rtnArr
+end
+
+def merge(hash_1, hash_2)
+    newHash = hash_1.merge(hash_2)
+end
+
+def censor(sentence, curses)
+    vowels = "aeiou"
+    sentence = sentence.split
+    sentence.each_with_index do |word,i|
+        if curses.include?(word) || curses.include?(word.capitalize) || curses.include?(word.upcase) || curses.include?(word.downcase)
+            word.each_char.with_index do |char,i|
+                if vowels.include?(char) || vowels.include?(char.upcase) || vowels.include?(char.downcase)
+                    word[i] = "*"
+                end
+            end
+            sentence[i] = word
+        end
+    end
+    sentence.join(" ")
+end
+
+puts censor("Gosh darn it", ["gosh", "darn", "shoot"])
